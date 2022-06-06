4932dcc14507c03bde1e41a4cfd8278874517579
tivyasrisaravanan <58386870+tivyasrisaravanan@users.noreply.github.com>
2022-06-06 14:08:02 +0800

    Update main.js

--- 3750/Animal-farm/main.js
+++ 3750/Animal-farm/main.js
@@ -16,39 +16,66 @@ var GameState = {
         this.scale.pageAlignHorizontally = true;
         this.scale.pageAlignVertically = true;
 
-        this.background = this.game.add.sprite(0, 0, 'background');
+        this.background = this.game.add.sprite(0, 0, 'background')
 
-        this.chicken = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'chicken');
-        this.chicken.anchor.setTo(0.5, 0.5);
-        this.chicken.scale.setTo(2,1);
+        var animalData = [
+            {key: 'chicken', text: 'CHICKEN'},
+            {key: 'horse', text: 'HORSE'},
+            {key: 'pig', text: 'PIG'},
+            {key: 'sheep', text: 'SHEEP'}
+        ];
 
-        this.horse = this.game.add.sprite(120, 10, 'horse');
-        this.horse.scale.setTo(0.5);
+        this.animals = this.game.add.group();
 
-        this.pig = this.game.add.sprite(500, 300, 'pig');
-        this.pig.anchor.setTo(0.5);
-        this.pig.scale.setTo(-1,1);
+        var self = this;
 
-        this.sheep = this.game.add.sprite(100, 250, 'sheep');
-        this.sheep.scale.setTo(0.5);
-        this.sheep.anchor.setTo(0.5);
+        animalData.forEach(function(element){
+            self.animals.create(200, self.game.world.centerY, element.key);
+        });
 
-        this.leftArrow = this.game.add.sprite(500, this.game.world.centerX, 'arrow');
+        //this.chicken = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'chicken');
+        //this.chicken.anchor.setTo(0.5, 0.5);
+        //this.chicken.scale.setTo(2,1);
+
+        //this.horse = this.game.add.sprite(120, 10, 'horse');
+        //this.horse.scale.setTo(0.5);
+
+        //this.pig = this.game.add.sprite(500, 300, 'pig');
+        //this.pig.anchor.setTo(0.5);
+        //this.pig.scale.setTo(-1,1);
+
+        //this.sheep = this.game.add.sprite(100, 250, 'sheep');
+        //this.sheep.scale.setTo(0.5);
+        //this.sheep.anchor.setTo(0.5);
+
+        this.leftArrow = this.game.add.sprite(60, this.game.world.centerY, 'arrow');
         this.leftArrow.anchor.setTo(0.5);
         this.leftArrow.scale.x = -1;
         this.leftArrow.customParams = {direction: 1};
 
-        this.rightArrow = this.game.add.sprite(500, this.game.world.centerY, 'arrow');
+        this.leftArrow.inputEnabled = true;
+        this.leftArrow.input.pixelPerfectClick = true;
+        this.leftArrow.events.onInputDown.add(this.switchAnimal, this);
+
+        this.rightArrow = this.game.add.sprite(580, this.game.world.centerY, 'arrow');
         this.rightArrow.anchor.setTo(0.5);
         this.rightArrow.customParams = {direction: 1};
     },
     update: function(){
-        this.sheep.angle += 0.5;
-     
+        //this.sheep.angle += 0.5;
     },
+    switchAnimal : function(sprite, event) {
+        console.log('move animal');
+    },
+    animateAnimal : function(sprite, event) {
+        console.log('animate animal');
+    }
+
 
    
-}
+};
+
+var game = new Phaser.Game(640, 360, Phaser.AUTO);
 
 game.state.add('GameState', GameState);
 game.state.start('GameState');
